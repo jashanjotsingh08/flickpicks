6117ab2330186c4fa6aedc6ae078ce9e54d31a22
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2023-03-27 02:26:02 -0400

    Updated App.js

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import PaperThemeProvider from './src/utils/styles/theme';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import Header from "./src/components/header/Header";
+import SearchResults from "./src/screens/SearchResults";
+
 
 export default function App() {
-  console.log('Test');
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigationRef = useNavigationContainerRef();
+  const Stack = createStackNavigator();
+
+  const handleSearch = async (query) => {
+    setSearchQuery(query);
+
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperThemeProvider>
+      <NavigationContainer ref={navigationRef} onReady={() => console.log('Navigation container is ready')}>
+        <Header searchQuery={searchQuery} handleSearch={handleSearch} navigationRef={navigationRef} />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="SearchResults">
+            {() => <SearchResults searchQuery={searchQuery} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperThemeProvider>
   );
 }
 
@@ -17,5 +40,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
