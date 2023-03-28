67e1df271fc49779ec0e382ed32b5dcdd3de4f58
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2023-03-28 03:09:55 -0400

    Updated Header.js styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,17 @@ import { StyleSheet } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import { TouchableOpacity } from 'react-native';
 import SearchBar from '../searchBar/SearchBar';
+import { useTheme } from 'react-native-paper';
 
 const Header = ({ searchQuery, setSearchQuery, navigationRef }) => {
+  const { glassmorphism, colors } = useTheme();
+
   const handleSearchBarPress = () => {
     navigationRef.navigate('SearchResults');
   };
   return (
-    <Appbar.Header style={styles.header}>
-      <Appbar.BackAction onPress={() => navigationRef.goBack()} />
+    <Appbar.Header style={{ ...glassmorphism, ...styles.header}}>
+      {/* <Appbar.BackAction onPress={() => navigationRef.goBack()} /> */}
       <TouchableOpacity onPress={handleSearchBarPress} style={styles.searchBar}>
         <SearchBar value={searchQuery} setSearchQuery={setSearchQuery} />
       </TouchableOpacity>
@@ -22,6 +25,9 @@ const styles = StyleSheet.create({
   header: {
     display: 'flex',
     flexDirection: 'row',
+    marginTop: 8,
+    marginHorizontal: 8,
+    borderRadius: 50,
   },
   searchBar: {
     flex: [1, 1, '100%'],
