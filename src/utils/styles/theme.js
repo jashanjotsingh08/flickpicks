05ca3527cb3bb229ad8b0056ae4911289554555f
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2023-03-28 03:08:32 -0400

    Updated theme.js

--- src/utils/styles/theme.js
+++ src/utils/styles/theme.js
@@ -1,38 +1,63 @@
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, MD3DarkTheme, Provider as PaperProvider } from 'react-native-paper';
+const glassmorphismShadow = {
+    shadowColor: '#000',
+    shadowOffset: {
+        width: -6,
+        height: -6,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+};
 
-const theme = {
+const glassmorphismBackground = {
+    backgroundColor: 'rgba(255, 255, 255, 0.15)',
+};
+
+const glassmorphismBorder = {
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.2)',
+};
+
+const glassmorphismStyle = {
+    ...glassmorphismShadow,
+    ...glassmorphismBackground,
+    ...glassmorphismBorder,
+    borderRadius: 16,
+};
+
+const lightTheme = {
     ...DefaultTheme,
+    roundness: 10,
     colors: {
         ...DefaultTheme.colors,
         primary: '#1B9CFC', // primary color
-        accent: '#55E6C1', // accent color
+        accent: '#ffffff',
+        background: '#d9d9d9',
+        surface: '#ffffff',
+        text: '#121212',
+        placeholder: '#888888',
     },
-    roundness: 4, // global border radius
-    components: {
-        Button: {
-            mode: 'contained',
-            style: {
-                borderRadius: 24,
-                height: 48,
-            },
-            labelStyle: {
-                fontWeight: 'bold',
-                fontSize: 16,
-                textTransform: 'uppercase',
-            },
-        },
-        TextInput: {
-            style: {
-                backgroundColor: '#F5F6FA', // background color
-                borderRadius: 8,
-                height: 56,
-            },
-            underlineColor: 'transparent',
-        },
-    },
+    glassmorphism: glassmorphismStyle,
 };
 
-export default function PaperThemeProvider({ children }) {
+const darkTheme = {
+    ...MD3DarkTheme,
+    roundness: 10,
+    colors: {
+        ...MD3DarkTheme.colors,
+        primary: '#1B9CFC', // primary color
+        accent: '#ffffff',
+        background: '#121212',
+        surface: '#333333',
+        text: '#ffffff',
+        placeholder: '#888888',
+    },
+    glassmorphism: glassmorphismStyle,
+};
+
+export default function PaperThemeProvider({ children, themeType }) {
+    const theme = themeType === 'dark' ? darkTheme : lightTheme;
+
     return (
         <PaperProvider theme={theme}>
             {children}
