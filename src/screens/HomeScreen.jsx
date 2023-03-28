9523b737bff3ac506dd5f50ad2bd62b3af8f4d8b
Jashan <31896439+jashanjotsingh08@users.noreply.github.com>
2023-03-28 03:14:34 -0400

    Updated HomeScreen component

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { ScrollView, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MovieSection from '../components/movieSection/MovieSection';
+import {
+  getLatestMovies,
+  getPopularMovies,
+  getNowPlayingMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from '../utils/api/movieService';
 
 const HomeScreen = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [latestMovies, setLatestMovies] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  useEffect(async () => {
+    setIsLoading(true);
+    getLatestMovies()
+      .then((data) => setLatestMovies([...data.results]))
+      .catch((err) => {
+        console.error(err);
+      });
+    getPopularMovies()
+      .then((data) => setPopularMovies([...data.results]))
+      .catch((err) => {
+        console.error(err);
+      });
+    getNowPlayingMovies()
+      .then((data) => setNowPlayingMovies([...data.results]))
+      .catch((err) => {
+        console.error(err);
+      });
+    getTopRatedMovies()
+      .then((data) => setTopRatedMovies([...data.results]))
+      .catch((err) => {
+        console.error(err);
+      });
+    getUpcomingMovies()
+      .then((data) => setUpcomingMovies([...data.results]))
+      .catch((err) => {
+        console.error(err);
+      });
+    setIsLoading(false);
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      {isLoading ? (
+        <></>
+      ) : (
+        <ScrollView style={styles.scrollView}>
+          {latestMovies && latestMovies.length > 0 && <MovieSection title='Latest' movies={latestMovies} />}
+          {popularMovies && <MovieSection title='Popular' movies={popularMovies} />}
+          {topRatedMovies && <MovieSection title='Top Rated' movies={topRatedMovies} />}
+          {nowPlayingMovies && <MovieSection title='Now Playing' movies={nowPlayingMovies} />}
+          {upcomingMovies && <MovieSection title='Upcoming' movies={upcomingMovies} />}
+        </ScrollView>
+      )}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    // paddingHorizontal: 6,
+    // paddingVertical: 6,
+  },
+});
 
 export default HomeScreen;
